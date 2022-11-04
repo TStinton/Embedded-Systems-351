ce61ab8dfadfb2e4cf1c21370c3a71c071e718db
TStinton <70481331+TStinton@users.noreply.github.com>
2022-11-04 16:00:56 +0000

    Challenge Done

--- Tasks/Task-370-Challenge/main.cpp
+++ Tasks/Task-370-Challenge/main.cpp
@@ -4,10 +4,41 @@ using namespace uop_msb;
 
 //Used this to 
 Ticker tick;
+Thread t1;
+osThreadId_t ReadADCThreadID;
+BufferedSerial serial(USBTX, USBRX, 115200);
 
+AnalogIn LDR(AN_LDR_PIN);
+DigitalOut GreenLed(TRAF_GRN1_PIN);
+unsigned short lightVal;
+int avg;
+int coun;
+
+void ReadADC(){
+    while(true){
+    ReadADCThreadID = ThisThread::get_id();
+    ThisThread::flags_wait_all(1);
+    //GreenLed = !GreenLed;
+    coun = coun+1;
+    lightVal = LDR.read_u16();
+    lightVal += lightVal;
+    
+    }
+}
+void Average();
+
+void ISR(){
+    osSignalSet(ReadADCThreadID,1);
+
+    //osSignalClear(ReadADCThreadID,1);
+}
 
 int main(void)
 {
+    tick.attach(&ISR,1ms);
+    t1.start(ReadADC);
+    //t1.join();
+
 /*
 1. Create an ISR - called by a Ticker every 1ms. This ISR simply has the job of signalling a waiting thread to perform an ADC conversion
 2. Create a thread that waits for a signal from the ISR. This thread should ideally have the highest priority
@@ -16,6 +47,10 @@ int main(void)
 */
     while (true) {
         ThisThread::sleep_for(1000ms);
+        avg = lightVal / coun;
+        lightVal = 0;
+        coun = 0;
+        printf("\nLDR = %d", avg);
     }
     
 }   
